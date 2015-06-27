90036961d4d3b9ef1662ac419ccbccef8e674db5
aashrai <ashrair@gmail.com>
2015-06-27 09:07:03 +0530

    added support for animations

--- library/src/main/java/snow/skittles/SkittleLayout.java
+++ library/src/main/java/snow/skittles/SkittleLayout.java
@@ -1,22 +1,29 @@
 package snow.skittles;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.content.Context;
 import android.support.v4.view.animation.FastOutLinearInInterpolator;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by aashrai on 17/6/15.
  */
-public class SkittleLayout extends FrameLayout implements View.OnClickListener {
+public class SkittleLayout extends FrameLayout implements View.OnClickListener, Animator.AnimatorListener {
 
     LinearLayout skittleContainer;
     ObjectAnimator animator;
+    int flag = 0;
+    List<Float> yList = new ArrayList<Float>();
 
     public SkittleLayout(Context context) {
         super(context);
@@ -54,18 +61,63 @@ public class SkittleLayout extends FrameLayout implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         View child;
-        ObjectAnimator animator;
-        FastOutLinearInInterpolator interpolator=new FastOutLinearInInterpolator();
+        FastOutLinearInInterpolator interpolator = new FastOutLinearInInterpolator();
+        Log.d("Skittle Layout", "" + skittleContainer.getChildCount());
 
+        if (flag == 0) {
+            flag = 1;
+            yList.clear();
+            for (int i = 0; i < skittleContainer.getChildCount(); i++) {
+                child = skittleContainer.getChildAt(i);
+                if (child.getId() != R.id.skittle_main) {
+                    yList.add(child.getY());
+                    animator = ObjectAnimator.ofPropertyValuesHolder(child,
+                            PropertyValuesHolder.ofFloat("Y", child.getY() + child.getMeasuredHeight() / 2, child.getY()),
+                            PropertyValuesHolder.ofFloat("alpha", 0, 1)).setDuration(200);
+                    animator.setInterpolator(interpolator);
+                    animator.start();
+                }
+            }
+        } else if (flag == 1) {
+            flag = 0;
+            for (int i = 0; i < skittleContainer.getChildCount(); i++) {
+                child = skittleContainer.getChildAt(i);
+                if (child.getId() != R.id.skittle_main) {
+                    animator = ObjectAnimator.ofPropertyValuesHolder(child,
+                            PropertyValuesHolder.ofFloat("Y", child.getY() + child.getMeasuredHeight() / 2, child.getY()),
+                            PropertyValuesHolder.ofFloat("alpha", 0, 1)).setDuration(200);
+                    animator.setInterpolator(interpolator);
+                    animator.addListener(this);
+                    animator.reverse();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        View child;
         for (int i = 0; i < skittleContainer.getChildCount(); i++) {
             child = skittleContainer.getChildAt(i);
             if (child.getId() != R.id.skittle_main) {
-                child.setVisibility(View.VISIBLE);
-                animator=ObjectAnimator.ofPropertyValuesHolder(child, PropertyValuesHolder.ofFloat("Y", child.getY() + 50
-                        , child.getY()), PropertyValuesHolder.ofFloat("alpha", 0, 1)).setDuration(300);
-                animator.setInterpolator(interpolator);
-                animator.start();
+                child.setY(yList.get(i));
             }
         }
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
     }
 }
