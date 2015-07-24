df0319c2ce2b93aac2f22dcba397a75f9280cd1e
aashrai <ashrair@gmail.com>
2015-07-24 23:54:41 +0530

    Using builder pattern in the api

--- skittles/src/main/java/snow/skittles/SkittleBuilder.java
+++ skittles/src/main/java/snow/skittles/SkittleBuilder.java
@@ -2,12 +2,16 @@ package snow.skittles;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DimenRes;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Utility class for managing the addition of skittles and click events
@@ -15,11 +19,11 @@ import android.view.View;
 @SuppressWarnings("ALL")
 public class SkittleBuilder {
 
-    SkittleLayout mSkittleLayout;
-    TextSkittle textSkittle;
-    Context context;
-    int skittleCount = 1;
-    Integer color;
+    private static SkittleLayout mSkittleLayout;
+    private TextSkittle textSkittle;
+    private Context context;
+    private int skittleCount = 1;
+    private int color;
 
     private SkittleClickListener mListener;
 
@@ -32,42 +36,9 @@ public class SkittleBuilder {
         void onTextSkittleClick(TextSkittle textSkittle);
     }
 
-    /**
-     * @param context        pass in the context of the activity
-     * @param mSkittleLayout pass an instance of the skittle you have set as the root of your layouts
-     * @param animatable     should the main Skittle have an animation
-     */
-    public SkittleBuilder(@NonNull Context context, @NonNull SkittleLayout mSkittleLayout, boolean animatable) {
-        this.mSkittleLayout = mSkittleLayout;
-        this.context = context;
-        mSkittleLayout.setMainSkittleAnimatable(animatable);
-    }
-
-    /**
-     * @param context        pass in the context of the activity
-     * @param mSkittleLayout pass an instance of the skittle you have set as the root of your layouts
-     * @param animatable     should the main Skittle have an animation
-     * @param color          pass the color for all mini skittles,you can pass any either a reference or a generated color
-     */
-    public SkittleBuilder(@NonNull Context context, @NonNull SkittleLayout mSkittleLayout, boolean animatable
-            , @Nullable Integer color) {
-
-        setUp(context, mSkittleLayout, animatable, color);
-
-    }
-
-    /**
-     * @param context
-     * @param mSkittleLayout
-     * @param animatable
-     * @param miniSkittleColor you can pass any either a reference or a generated color
-     * @param mainSkittleColor pass the for the main Skittle,you can pass any either a reference or a generated color
-     */
-    public SkittleBuilder(@NonNull Context context, @NonNull SkittleLayout mSkittleLayout, boolean animatable
-            , @Nullable Integer miniSkittleColor, int mainSkittleColor) {
-
-        setUp(context, mSkittleLayout, animatable, miniSkittleColor);
-        mSkittleLayout.setMainSkittleColor(mainSkittleColor);
+    private SkittleBuilder(Builder builder) {
+        setUp(builder.mContext, builder.mSkittleLayout, builder.animatable, builder.miniSkittleColor);
+        mSkittleLayout.setMainSkittleColor(builder.mainSkittleColor);
     }
 
     private void setUp(@NonNull Context context, @NonNull SkittleLayout mSkittleLayout, boolean animatable
@@ -128,10 +99,7 @@ public class SkittleBuilder {
         skittle.setImageDrawable(context.getResources().getDrawable(icon));
         skittle.setAlpha(0f);
         skittle.setClickability(false);
-
-        if (color != null) {
-            skittle.setBackgroundTintList(Utils.getColorStateList(color, context));
-        }
+        skittle.setBackgroundTintList(Utils.getColorStateList(color, context));
 
         skittle.setOnClickListener(mSkittleClickListener);
         skittle.setPosition(skittleCount++);
@@ -139,50 +107,18 @@ public class SkittleBuilder {
         return skittle;
     }
 
+    public TextSkittleBuilder makeTextSkittle(String text, @DrawableRes int icon) {
+        return new TextSkittleBuilder(text, icon);
+    }
 
-    /**
-     * Call this method after @see #makeTextSkittle(Drawable,String) and modifying the look of
-     * the skittle
-     *
-     * @param textSkittle
-     */
 
-    public void addTextSkittle(@NonNull TextSkittle textSkittle) {
+    private void addTextSkittle(TextSkittle textSkittle) {
 
-        textSkittle.getTextSkittle().setAlpha(0f);
+        textSkittle.setAlpha(0f);
         textSkittle.setPosition(skittleCount++);
-        mSkittleLayout.addFab(textSkittle.getTextSkittle());
+        mSkittleLayout.addFab(textSkittle);
         textSkittle.getSkittle().setOnClickListener(mTextSkittleClickListener);
-
     }
-
-    /**
-     * Call this method first while adding the text skittle
-     * exposes rich methods for modifications,follow with a
-     * call to @see #addTextSkittle(TextSkittle)
-     *
-     * @param icon
-     * @param text
-     * @return
-     */
-    public TextSkittle makeTextSkittle(@Nullable Integer icon, @NonNull String text) {
-
-        textSkittle = new TextSkittle(context, mSkittleLayout.getSkittleContainer());
-        textSkittle.setIcon(context.getResources().getDrawable(icon));
-        textSkittle.setText(text);
-
-        return textSkittle;
-    }
-
-    public TextSkittle makeTextSkittle(@Nullable Integer icon, @NonNull String text, int color) {
-
-
-        textSkittle = makeTextSkittle(icon, text);
-        textSkittle.setSkittleColor(color);
-
-        return textSkittle;
-    }
-
 
     View.OnClickListener mSkittleClickListener = new View.OnClickListener() {
         @Override
@@ -200,7 +136,7 @@ public class SkittleBuilder {
             Log.d("Skittles", "Text Skittle Click");
             Skittle skittle = (Skittle) v;
             if (v.isClickable() && getmListener() != null)
-                getmListener().onTextSkittleClick(new TextSkittle((TextSkittleContainer) v.getParent()));
+                getmListener().onTextSkittleClick((TextSkittle) v.getParent());
         }
     };
 
@@ -222,5 +158,98 @@ public class SkittleBuilder {
      */
     public void toggleSkittleMenu() {
         mSkittleLayout.animateMiniSkittles();
+    }
+
+    /**
+     * Convenient builder pattern for instantiating the SkittleBuilder
+     */
+    public static class Builder {
+
+        private final SkittleLayout mSkittleLayout;
+        private final Context mContext;
+        private boolean animatable;
+        private int mainSkittleColor, miniSkittleColor;
+        private Drawable mainSkittleIcon;
+
+        public Builder(Context mContext, SkittleLayout mSkittleLayout) {
+            this.mContext = mContext;
+            this.mSkittleLayout = mSkittleLayout;
+        }
+
+        public Builder animatable(boolean animatable) {
+            this.animatable = animatable;
+            return this;
+        }
+
+        public Builder mainSkittleColor(int color) {
+            this.mainSkittleColor = color;
+            return this;
+        }
+
+        public Builder miniSkittleColor(int color) {
+            this.miniSkittleColor = color;
+            return this;
+        }
+
+        public Builder mainSkittleIcon(@DrawableRes int iconRes) {
+            this.mainSkittleIcon = mContext.getResources().getDrawable(iconRes);
+            return this;
+        }
+
+        public SkittleBuilder build() {
+            return new SkittleBuilder(this);
+        }
+    }
+
+    /**
+     * Convenient builder pattern for creating @link snow.skittles.TextSkittle
+     */
+    public class TextSkittleBuilder {
+        private final TextView textView;
+        private final FloatingActionButton miniSkittle;
+        private final TextSkittle textSkittle;
+
+        public TextSkittleBuilder(String text, @DrawableRes int icon) {
+            textSkittle = (TextSkittle) LayoutInflater.from(context)
+                    .inflate(R.layout.text_action_skittle, mSkittleLayout.getSkittleContainer(), false);
+            textView = textSkittle.getTextView();
+            textView.setText(text);
+            miniSkittle = textSkittle.getSkittle();
+            miniSkittle.setImageDrawable(context.getResources().getDrawable(icon));
+        }
+
+        public TextSkittleBuilder setTextColor(@ColorRes int textColor) {
+            textView.setTextColor(context.getResources().getColor(textColor));
+            return this;
+        }
+
+        public TextSkittleBuilder setTextSize(@DimenRes int size) {
+            textView.setTextSize(context.getResources().getDimensionPixelSize(size));
+            return this;
+        }
+
+        public TextSkittleBuilder setTextBackgroundColor(@ColorRes int backgroundColor) {
+            textView.setBackgroundColor(context.getResources().getColor(backgroundColor));
+            return this;
+        }
+
+        public TextSkittleBuilder setTextBackground(@DrawableRes int drawable) {
+            textView.setBackground(context.getResources().getDrawable(drawable));
+            return this;
+        }
+
+        public TextSkittleBuilder setSkittleColor(int color) {
+            miniSkittle.setBackgroundTintList(Utils.getColorStateList(color, context));
+            return this;
+        }
+
+        public TextSkittleBuilder setSkittleIcon(@DrawableRes int drawable) {
+            miniSkittle.setImageDrawable(context.getResources().getDrawable(drawable));
+            return this;
+        }
+
+        public void add() {
+            addTextSkittle(textSkittle);
+        }
     }
 }
