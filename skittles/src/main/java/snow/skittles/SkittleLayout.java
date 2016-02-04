c8251c6c24818e5fb3fe90dfeb91c69c3fe3cb83
aashrai <ashrair@gmail.com>
2016-02-04 17:46:30 +0530

    Ensure SkittleContainer is on top

--- skittles/src/main/java/snow/skittles/SkittleLayout.java
+++ skittles/src/main/java/snow/skittles/SkittleLayout.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by aashrai on 2/2/16.
@@ -36,6 +39,7 @@ public class SkittleLayout extends CoordinatorLayout {
     } finally {
       array.recycle();
     }
+
     skittleContainer = (SkittleContainer) LayoutInflater.from(context)
         .inflate(R.layout.skittle_container, this, false);
     SkittleAdapter skittleAdapter = new SkittleAdapter(color, drawable);
@@ -45,6 +49,34 @@ public class SkittleLayout extends CoordinatorLayout {
     skittleContainer.setItemAnimator(new ItemAnimator());
     skittleContainer.addItemDecoration(new SkittleDecorator());
     addView(skittleContainer);
+  }
+
+  @Override public void addView(View child) {
+    super.addView(child);
+    if (getChildCount() != 1 && !(child instanceof Snackbar.SnackbarLayout)) addSkittleOnTop();
+  }
+
+  @Override public void addView(View child, int index) {
+    super.addView(child, index);
+    if (getChildCount() != 1 && !(child instanceof Snackbar.SnackbarLayout)) addSkittleOnTop();
+  }
+
+  @Override public void addView(View child, int width, int height) {
+    super.addView(child, width, height);
+    if (getChildCount() != 1) addSkittleOnTop();
+  }
+
+  @Override public void addView(View child, ViewGroup.LayoutParams params) {
+    super.addView(child, params);
+    if (getChildCount() != 1) addSkittleOnTop();
+  }
+
+  /**
+   * Utility method called after addView to ensure the skittle container is on top
+   */
+  private void addSkittleOnTop() {
+    detachViewFromParent(skittleContainer);
+    attachViewToParent(skittleContainer, getChildCount(), skittleContainer.getLayoutParams());
   }
 
   public SkittleAdapter getSkittleAdapter() {
